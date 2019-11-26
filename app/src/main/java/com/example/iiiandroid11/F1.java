9885147cd8e07474edbca06cf0003d7ddb44be2c
brad <brad@brad.tw>
2019-11-26 14:30:41 +0800

    2

---
+++ app/src/main/java/com/example/iiiandroid11/F1.java
@@ -0,0 +1,25 @@
+package com.example.iiiandroid11;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class F1 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_f1, container, false);
+    }
+
+}
