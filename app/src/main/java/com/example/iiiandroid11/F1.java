717617e85529acb5c0f43ab1860d230e45db932c
brad <brad@brad.tw>
2019-11-26 15:12:00 +0800

    3

--- app/src/main/java/com/example/iiiandroid11/F1.java
+++ app/src/main/java/com/example/iiiandroid11/F1.java
@@ -5,21 +5,38 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class F1 extends Fragment {
+    private View mainView;
+    private TextView lottery;
+    private Button btnLottery;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_f1, container, false);
+        mainView = inflater.inflate(R.layout.fragment_f1, container, false);
+
+        lottery = mainView.findViewById(R.id.f1_lottery);
+        btnLottery = mainView.findViewById(R.id.f1_btn);
+        btnLottery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                lottery.setText("" + (int)(Math.random()*49+1));
+            }
+        });
+
+        return mainView;
     }
+
 
 }
--- app/src/main/java/com/example/iiiandroid11/MainActivity.java
+++ app/src/main/java/com/example/iiiandroid11/MainActivity.java
@@ -1,14 +1,42 @@
 package com.example.iiiandroid11;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private F1 f1;
+    private F2 f2;
+    private FragmentManager fmgr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        f1 = new F1(); f2 = new F2();
+
+        fmgr = getSupportFragmentManager();
+        FragmentTransaction transaction = fmgr.beginTransaction();
+        transaction.add(R.id.container,f1);
+        transaction.commit();
+
+
     }
+
+    public void toF1(View view) {
+        FragmentTransaction transaction = fmgr.beginTransaction();
+        transaction.replace(R.id.container, f1);
+        transaction.commit();
+    }
+    public void toF2(View view) {
+        FragmentTransaction transaction = fmgr.beginTransaction();
+        transaction.replace(R.id.container, f2);
+        transaction.commit();
+    }
+
 }
